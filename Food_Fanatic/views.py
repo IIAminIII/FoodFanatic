1b529786bad43639ae2109530696f33f93617e77
Aminul Islam <aminalislam4223@gmail.com>
2026-07-29 10:50:53 +0600

    chore(observability): log menu database source

--- Food_Fanatic/views.py
+++ Food_Fanatic/views.py
@@ -1,6 +1,13 @@
+import logging
+
+from django.conf import settings
+from django.db import connection
 from django.shortcuts import get_object_or_404, render
 
 from menu.models import Category, FoodItem
+
+
+logger = logging.getLogger(__name__)
 
 
 def home(request, category_slug=None):
@@ -8,6 +15,13 @@ def home(request, category_slug=None):
     if category_slug is not None:
         category = get_object_or_404(Category, slug=category_slug)
         food_items = food_items.filter(category=category)
+
+    logger.info(
+        "Menu request backend=%s host=%s available_items=%s",
+        connection.vendor,
+        settings.DATABASES["default"].get("HOST", ""),
+        food_items.count(),
+    )
 
     return render(
         request,
